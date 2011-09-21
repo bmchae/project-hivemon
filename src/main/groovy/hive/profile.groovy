57371f621a9a99ca95ba71f40a6e8fd469c9e34a
bmchae <bmchae@gmail.com>
2011-09-21 10:32:12 +0900

    modify regexp

--- src/main/groovy/hive/profile.groovy
+++ src/main/groovy/hive/profile.groovy
@@ -30,8 +30,8 @@ def parse(f) {
 				//printf '         * %s | %s | %s\n', new java.text.SimpleDateFormat('HH:mm:ss').format(Long.parseLong(m[0][3])),
 				//									md5(m[0][1]),
 				//                                    m[0][1].trim().substring(0, Math.min(50, m[0][1].trim().size()))
-				//startTime = Long.parseLong(m[0][3])
-				println 'start time : ' + startTime
+				startTime = Long.parseLong(m[0][3])
+				//println 'start time : ' + startTime
 			    break
 			case ~/^QueryEnd QUERY_STRING=.+/ :
 				m = line =~ /^QueryEnd QUERY_STRING="([^\"]+)"[ ]+QUERY_ID="([^\"]+)"[ ]+.+TIME="([^\"]+)"/
