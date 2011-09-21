bba38d74808358b6b4686467a689e5fca6fd2ee6
bmchae <bmchae@gmail.com>
2011-09-21 10:31:14 +0900

    modify regexp

--- src/main/groovy/hive/profile.groovy
+++ src/main/groovy/hive/profile.groovy
@@ -20,26 +20,29 @@ files.each { f ->
 
 
 def parse(f) {
-	def jobMap = [:]
-	
-	def task = null
+	def queryMap = [:]
 		
 	def startTime = 0L
 	f.eachLine { line ->
 		switch(line) {
 			case ~/^QueryStart QUERY_STRING=.+/ :
-				m = line =~ /^QueryStart QUERY_STRING="(.+)"[ ]+QUERY_ID="(.+)"[ ]+TIME="(.+)"/
+				m = line =~ /^QueryStart QUERY_STRING="([^\"]+)"[ ]+QUERY_ID="([^\"]+)"[ ]+TIME="([^\"]+)"/
 				//printf '         * %s | %s | %s\n', new java.text.SimpleDateFormat('HH:mm:ss').format(Long.parseLong(m[0][3])),
 				//									md5(m[0][1]),
 				//                                    m[0][1].trim().substring(0, Math.min(50, m[0][1].trim().size()))
-				startTime = Long.parseLong(m[0][3])
+				//startTime = Long.parseLong(m[0][3])
+				println 'start time : ' + startTime
 			    break
 			case ~/^QueryEnd QUERY_STRING=.+/ :
-				m = line =~ /^QueryEnd QUERY_STRING="(.+)"[ ]+QUERY_ID="(.+)"[ ]+TIME="(.+)"/
-				printf '         * %s | %10s | %s | %s\n', new java.text.SimpleDateFormat('HH:mm:ss').format(startTime),
-				                                    Long.parseLong(m[0][3]) - startTime,
-													md5(m[0][1]),
-				                                    m[0][1].trim().substring(0, Math.min(50, m[0][1].trim().size()))
+				m = line =~ /^QueryEnd QUERY_STRING="([^\"]+)"[ ]+QUERY_ID="([^\"]+)"[ ]+.+TIME="([^\"]+)"/
+				if (m.size() < 1 && m[0].size() < 4)
+					break
+				//println 'end   time : ' + m[0][3]
+				printf '         * %s | %10s | %s | %s\n', 
+						new java.text.SimpleDateFormat('HH:mm:ss').format(startTime),
+                        Long.parseLong(m[0][3]) - startTime,
+						md5(m[0][1]),
+                        m[0][1].trim().substring(0, Math.min(50, m[0][1].trim().size()))
 			    break
 			default:
 			    break
