f5db4ea42f0f3b3dc1ca3a5663a02f3e49804647
bmchae <bmchae@gmail.com>
2011-09-23 13:39:49 +0900

    .

--- src/main/groovy/hadoop/monitory/monitor.groovy
+++ src/main/groovy/hadoop/monitory/monitor.groovy
@@ -6,7 +6,7 @@ output.eachLine { line ->
 	if (m.size() > 0 && m[0].size() > 3) {
 		if (jps[m[0][1]] == null)
 			jps[m[0][1]] = []
-		if (jps[m[0][1]] == /(GroovyStarter|Jps)/)
+		if (jps[m[0][1]] =~ /(GroovyStarter|Jps)/)
 			return
 		jps[m[0][1]] << (m[0][3] == '' ? 'X' : m[0][3])
 	}
