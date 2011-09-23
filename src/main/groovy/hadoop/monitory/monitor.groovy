24c463041e4f50d14fc477d90ac307b18c775c03
bmchae <bmchae@gmail.com>
2011-09-23 23:08:34 +0900

    .

--- src/main/groovy/hadoop/monitory/monitor.groovy
+++ src/main/groovy/hadoop/monitory/monitor.groovy
@@ -1,6 +1,22 @@
-def output = 'all jps'.execute().text
+//Process p = Runtime.getRuntime().exec('sh -c \"all jps\"')
+//Process p = Runtime.getRuntime().exec('all jps')
+//p.waitFor()
+
+//def output = """sh -c 'all jps'""".execute().waitFor().text
+
+//def initialSize = 4096
+//def os = new ByteArrayOutputStream(initialSize)
+//def err = new ByteArrayOutputStream(initialSize)
+def p = """all jps""".execute()
+//def t = p.consumeProcessOutputStream(os)
+//p.waitForProcessOutput()
+//t.join()
+def output = p.text
 
 def jps = [:]
+//BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//String line;
+//while ((line = br.readLine()) != null) {
 output.eachLine { line ->
 	def m = line =~ /^(.*?):\s+(.*?)\s+(.*?)$/
 	if (m.size() > 0 && m[0].size() > 3) {
