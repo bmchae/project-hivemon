6fcff2d8494ce6ebd51230afa8e19790dba6995c
bmchae <bmchae@gmail.com>
2011-09-27 16:32:44 +0900

    9.27

--- src/main/groovy/hadoop/monitor/monitor.groovy
+++ src/main/groovy/hadoop/monitor/monitor.groovy
@@ -1,30 +1,21 @@
-//Process p = Runtime.getRuntime().exec('sh -c \"all jps\"')
-//Process p = Runtime.getRuntime().exec('all jps')
-//p.waitFor()
-
-//def output = """sh -c 'all jps'""".execute().waitFor().text
-
-//def initialSize = 4096
-//def os = new ByteArrayOutputStream(initialSize)
-//def err = new ByteArrayOutputStream(initialSize)
 def p = """all jps""".execute()
-//def t = p.consumeProcessOutputStream(os)
-//p.waitForProcessOutput()
-//t.join()
-def output = p.text
-
+p.waitFor()
+def output = p.in.text
 def jps = [:]
-//BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//String line;
-//while ((line = br.readLine()) != null) {
 output.eachLine { line ->
 	def m = line =~ /^(.*?):\s+(.*?)\s+(.*?)$/
 	if (m.size() > 0 && m[0].size() > 3) {
 		if (jps[m[0][1]] == null)
 			jps[m[0][1]] = []
-		if (jps[m[0][1]] =~ /(GroovyStarter|Jps)/)
-			return
-		jps[m[0][1]] << (m[0][3] == '' ? 'X' : m[0][3])
+		switch (m[0][3]) {
+	    case 'GroovyStarter' :
+	    case 'Jps' :
+	    case '' :
+		    break
+		default :
+		    jps[m[0][1]] << m[0][3]
+			break
+		}
 	}
 }
 
