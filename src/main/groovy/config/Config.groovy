ed7348d10933cf7ba3509f6f123dc17c12cc6c8b
bmchae <bmchae@gmail.com>
2011-09-27 15:09:00 +0900

    9.27

--- src/main/groovy/config/Config.groovy
+++ src/main/groovy/config/Config.groovy
@@ -1,6 +1,7 @@
 package config
 
 class Config {
-	static HIVE_LOG_DIR = "/tmp/" + System.getProperty('user.name')
+	static IS_LINUX = 'uname -s'.execute().text == 'Linux' ? true : false
+ 	static HIVE_LOG_DIR = "/tmp/" + System.getProperty('user.name')
 	static HADOOP_LOG_DIR = System.getenv()['HADOOP_LOG_DIR'] != null ? System.getenv()['HADOOP_LOG_DIR'] : System.getenv()['HADOOP_HOME'] + '/logs'
 }
