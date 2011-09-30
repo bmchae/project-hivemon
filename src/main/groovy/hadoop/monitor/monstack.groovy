8f558c531c4281f86d4f12d0f7761aa5807ead41
bmchae <bmchae@gmail.com>
2011-09-30 15:44:27 +0900

    9.30

--- src/main/groovy/hadoop/monitor/monstack.groovy
+++ src/main/groovy/hadoop/monitor/monstack.groovy
@@ -7,8 +7,8 @@ output.eachLine { line ->
 	if (m.size() > 0 && m[0].size() > 3) {
 		if (jps[m[0][1]] == null)
 			jps[m[0][1]] = []
-		def p = "ssh nexr@${m[0][1]} jstack ${m[0][2]}".execute()
-		p.waitFor()
-		println p.in.text
+		def pp = "ssh nexr@${m[0][1]} jstack ${m[0][2]}".execute()
+		pp.waitFor()
+		println pp.in.text
 	}
 }
