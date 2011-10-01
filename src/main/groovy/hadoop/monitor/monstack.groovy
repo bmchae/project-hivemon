e393695a252faad2dd391a72d0aeffab77990117
bmchae <bmchae@gmail.com>
2011-10-01 15:48:30 +0900

    10-01

--- src/main/groovy/hadoop/monitor/monstack.groovy
+++ src/main/groovy/hadoop/monitor/monstack.groovy
@@ -14,11 +14,12 @@ output.eachLine { line ->
 		def pp = "ssh ${m[0][1]} \". ~/.bash_profile; jstack ${m[0][2]} 2>&1\"".execute()
 		//def pp = "jstack ${m[0][2]}".execute()
 		pp.waitFor()
+		println pp.in.text
 		
 		//def mm = (pp.in.text =~ /(?m)"main"(.+?)^$/)
-		def mm = java.util.regex.Pattern.compile(/(?m)"main"(.+?)^$/, java.util.regex.Pattern.MULTILINE | java.util.regex.Pattern.DOTALL).matcher(pp.in.text)
+		//def mm = java.util.regex.Pattern.compile(/(?m)"main"(.+?)^$/, java.util.regex.Pattern.MULTILINE | java.util.regex.Pattern.DOTALL).matcher(pp.in.text)
 
-		if (mm.size() > 0)
-			println mm[0][0]
+		//if (mm.size() > 0)
+		//	println mm[0][0]
 	}
 }
