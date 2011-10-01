e78f43d6d9257d458f499dbc580b96260a87665d
bmchae <bmchae@gmail.com>
2011-10-01 21:46:08 +0900

    10-01 - dummy comment

--- src/main/groovy/config/Config.groovy
+++ src/main/groovy/config/Config.groovy
@@ -3,6 +3,5 @@ package config
 class Config {
 	static IS_LINUX = 'uname -s'.execute().text == 'Linux' ? true : false
  	static HIVE_LOG_DIR = "/tmp/" + System.getProperty('user.name')
-	//static HADOOP_LOG_DIR = System.getenv()['HADOOP_LOG_DIR'] != null ? System.getenv()['HADOOP_LOG_DIR'] : System.getenv()['HADOOP_HOME'] + '/logs'
-	static HADOOP_LOG_DIR = System.getenv()['HADOOP_LOG_DIR'] != null ? System.getenv()['HADOOP_LOG_DIR'] : '/tmp/nexr'
+	static HADOOP_LOG_DIR = System.getenv()['HADOOP_LOG_DIR'] != null ? System.getenv()['HADOOP_LOG_DIR'] : System.getenv()['HADOOP_HOME'] + '/logs'
 }
