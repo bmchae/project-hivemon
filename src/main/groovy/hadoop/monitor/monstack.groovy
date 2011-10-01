fd59060f17c09d4add2110c8a518db8d97106733
bmchae <bmchae@gmail.com>
2011-10-01 16:00:31 +0900

    10-01

--- src/main/groovy/hadoop/monitor/monstack.groovy
+++ src/main/groovy/hadoop/monitor/monstack.groovy
@@ -11,7 +11,7 @@ output.eachLine { line ->
 
 		println " ${m[0][1]} | ${m[0][3]} - ${m[0][2]} ".center(100, '~')
 
-		def pp = "ssh ${m[0][1]} \". ~/.bash_profile; jstack -l ${m[0][2]}\" 2>&1".execute()
+		def pp = "ssh ${m[0][3]} \". ~/.bash_profile; jstack -l ${m[0][2]}\" 2>&1".execute()
 		//def pp = "ssh ${m[0][1]} \". ~/.bash_profile; jps\" 2>&1".execute()
 		//def pp = "jstack ${m[0][2]}".execute()
 		pp.waitFor()
