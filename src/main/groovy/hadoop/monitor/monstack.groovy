8da74a27b486998ce7242144b51a4aa84c1e3f6c
bmchae <bmchae@gmail.com>
2011-10-01 14:54:00 +0900

    10.1

--- src/main/groovy/hadoop/monitor/monstack.groovy
+++ src/main/groovy/hadoop/monitor/monstack.groovy
@@ -8,11 +8,13 @@ output.eachLine { line ->
 	if (m.size() > 0 && m[0].size() > 3) {
 		if (jps[m[0][1]] == null)
 			jps[m[0][1]] = []
-		//def pp = "ssh nexr@${m[0][1]} jstack ${m[0][2]}".execute()
-		def pp = "jstack ${m[0][2]}".execute()
+
+		println " ${m[0][1]} | ${m[0][3]} - ${m[0][2]} ".center(100, '~')
+
+		def pp = "ssh nexr@${m[0][1]} jstack ${m[0][2]} 2>&1".execute()
+		//def pp = "jstack ${m[0][2]}".execute()
 		pp.waitFor()
 		
-		println " ${m[0][1]} | ${m[0][3]} - ${m[0][2]} ".center(100, '~')
 		//def mm = (pp.in.text =~ /(?m)"main"(.+?)^$/)
 		def mm = java.util.regex.Pattern.compile(/(?m)"main"(.+?)^$/, java.util.regex.Pattern.MULTILINE | java.util.regex.Pattern.DOTALL).matcher(pp.in.text)
 
