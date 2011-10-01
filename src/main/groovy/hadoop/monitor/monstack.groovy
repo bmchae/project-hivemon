c3299b4d5eb8e9b66f46648728a987af7d7f0799
bmchae <bmchae@gmail.com>
2011-10-01 14:23:35 +0900

    10.1

--- src/main/groovy/hadoop/monitor/monstack.groovy
+++ src/main/groovy/hadoop/monitor/monstack.groovy
@@ -1,5 +1,5 @@
 //def p = """all jps | grep Child""".execute()
-def p = """allsync jps | grep Tracker""".execute()
+def p = """all jps | grep TaskTracker""".execute()
 p.waitFor()
 def output = p.in.text
 def jps = [:]
@@ -8,15 +8,15 @@ output.eachLine { line ->
 	if (m.size() > 0 && m[0].size() > 3) {
 		if (jps[m[0][1]] == null)
 			jps[m[0][1]] = []
-		def pp = "ssh nexr@${m[0][1]} jstack ${m[0][2]}".execute()
+		//def pp = "ssh nexr@${m[0][1]} jstack ${m[0][2]}".execute()
+		def pp = "jstack ${m[0][2]} ".execute()
 		pp.waitFor()
 		
 		println " ${m[0][1]} - ${m[0][2]} ".center(100, '~')
-		println pp.in.text
-		//def mm = (pp.in.text =~ /"main"(.+?)\r?\n/)
-		//def mm = java.util.regex.Pattern.compile(/"main"(.+?)\r?\n/, java.util.regex.Pattern.MULTILINE).matcher(pp.in.text)
+		//def mm = (pp.in.text =~ /(?m)"main"(.+?)^$/)
+		def mm = java.util.regex.Pattern.compile(/(?m)"main"(.+?)^$/, java.util.regex.Pattern.MULTILINE | java.util.regex.Pattern.DOTALL).matcher(pp.in.text)
 
-		//if (mm.size() > 0)
-		//	println mm[0]
+		if (mm.size() > 0)
+			println mm[0][0]
 	}
 }
