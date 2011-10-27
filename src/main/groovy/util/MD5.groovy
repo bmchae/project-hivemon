06f19ac922e4772b9842f54e0c0172776d37d12e
Peter Chae <bmchae@gmail.com>
2011-10-27 12:55:52 +0900

    10/27 -

--- src/main/groovy/util/MD5.groovy
+++ src/main/groovy/util/MD5.groovy
@@ -4,7 +4,7 @@ package util
 class MD5 {
 	def static md5(String plain) {
 		java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5")
-		md.update(plain.bytes)
+		md.update(plain.replaceAll('\\s+', '').bytes)
 		BigInteger big = new BigInteger(1, md.digest())
 		return big.toString(16).padLeft(32,"0")
 	}
